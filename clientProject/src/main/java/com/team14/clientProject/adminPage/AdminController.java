ab9ebf4ea434148fb03bb7d9655fc99de82963e7
Mohammed Bolaji <bolajims@cardiff.ac.uk>
2024-12-19 14:39:53 +0000

    Adding Validation To the admin page for adding new users

--- clientProject/src/main/java/com/team14/clientProject/adminPage/AdminController.java
+++ clientProject/src/main/java/com/team14/clientProject/adminPage/AdminController.java
@@ -1,8 +1,10 @@
 package com.team14.clientProject.adminPage;
 
 import com.team14.clientProject.loggingSystem.*;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -70,7 +72,11 @@ public class AdminController {
     }
 
     @PostMapping("/admin/add")
-    public String addUser(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
+    public String addUser(@ModelAttribute @Valid User user, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("error", "Invalid data: " + result.getAllErrors().get(0).getDefaultMessage());
+            return "redirect:/admin/add";
+        }
         try {
             adminService.addUser(user);
             redirectAttributes.addFlashAttribute("success", "User added successfully!");
